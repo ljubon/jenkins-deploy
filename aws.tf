8c393b4177628b36e2c7ccb120521d27575d19d7
Ljubo Nikolic <ljubonikolic@gmail.com>
2017-08-31 15:20:54 +0200

    edit key-name ljubon-key -> ljubon-key-jenkins

--- aws.tf
+++ aws.tf
@@ -4,7 +4,7 @@ provider "aws" {
     region 		= "${var.aws_region}"
 }
 
-resource "aws_key_pair" "ljubon-key" {
+resource "aws_key_pair" "ljubon-key-jenkins" {
   key_name   = "${var.key_name}"
   public_key = "${file(var.public_key_path)}"
 }
