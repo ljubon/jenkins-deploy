3628b6d3ecc80cdb8e2d1d65b56b9e94c7145b16
Nina <ljubonikolic@gmail.com>
2017-09-03 16:18:14 +0200

    edit install.sh

--- script/install.sh
+++ script/install.sh
@@ -9,6 +9,31 @@ sudo rm -rf epel-release-7-10.noarch.rpm
 sudo cd $HOME
 echo "*****************FINISH EPEL RELEASE*****************"
 
+
+
+echo "*****************CONFIGURE RHEL*****************"
+echo "Change hostname"
+sudo hostnamectl set-hostname jenkins-server
+echo "New hostname is: $(echo hostnamectl status)"
+
+
+
+
+# sudo hostname nat - this is for nat instance
+# sudo hostnamectl set-hostname web-server - this is for web server
+# sudo hostnamectl set-hostname db-server - this is for db server
+
+# from jenkins
+# !! ssh-keyscan [web-ip] >> ~/.ssh/known_hosts
+# !! ssh-keyscan [nat-ip] >> ~/.ssh/known_hosts
+# from web
+# !! ssh-keyscan [jenkins-ip] >> ~/.ssh/known_hosts
+# !! ssh-keyscan [nat-ip] >> ~/.ssh/known_hosts
+# from nat
+# !! ssh-keyscan [jenkins-ip] >> ~/.ssh/known_hosts
+# !! ssh-keyscan [web-ip] >> ~/.ssh/known_hosts
+
+
 # sudo usermod -a -G root jenkins
 # sudo vi /etc/sysconfig/jenkins
 # change JENKINS_LISTEN_ADDRESS="0.0.0.0"
