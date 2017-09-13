d5a85e29b0b20ef104b62b066a5038faef7f11bb
Ljubo Nikolic <ljubonikolic@gmail.com>
2017-09-13 19:58:09 +0200

    test

--- aws.tf
+++ aws.tf
@@ -9,8 +9,7 @@ resource "aws_key_pair" "ljubon-key-jenkins" {
   key_name   = "${var.key_name}"
   public_key = "${file(var.public_key_path)}"
 }
-
-
+# add test
 # RHEL instance for Jenkins server
 resource "aws_instance" "jenkins" {
 	ami = "${var.amis}"
